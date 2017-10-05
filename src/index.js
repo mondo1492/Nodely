2dc36a70e5acd61535937637270eee823e4284fe
Aaron Mondshine <aaron.mondshine@gmail.com>
2017-10-05 09:03:27 -0700

    setup fresh frontend JS project

---
+++ src/index.js
@@ -0,0 +1,17 @@
+// import _ from 'lodash';
+//
+// function component() {
+//   var element = document.createElement('div');
+//
+//   // Lodash, currently included via a script, is required for this line to work
+//   element.innerHTML = _.join(['Hellod', 'webpack'], ' ');
+//
+//   return element;
+// }
+//
+// document.body.appendChild(component());
+
+class Nodely {
+  constructor() {
+  }
+}
